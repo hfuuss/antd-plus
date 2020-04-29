d5dfa6f01182f93d02e427d8e0239e28b670d970
zhanlipeng <zhanlipeng2014@xiaochuankeji.cn>
2020-04-29 15:15:31 +0800

    feat(doc): 更新站点

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'antd-plus',
   mode: 'site',
+  navs: [
+    null,
+    { title: 'GitHub', path: 'https://github.com/hfuuss/antd-plus' },
+    { title: '更新日志', path: 'https://github.com/hfuuss/antd-plus/releases' },
+  ],
   // more config: https://d.umijs.org/config
 });
